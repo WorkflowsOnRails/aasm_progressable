d2445be8a0cd04fe5c6e13d6f62c610f116ae5d8
Brendan MacDonell <brendan@macdonell.net>
2014-01-04 02:12:51 -0500

    Add some source code documentation.

--- app/helpers/aasm_progressable/helper.rb
+++ app/helpers/aasm_progressable/helper.rb
@@ -1,22 +1,32 @@
-#
+# The ActionView helper. This should be made accessible to appropriate
+# controllers by adding a `helper AasmProgressable::Helper` declaration
+# to them.
 #
 # @author Brendan MacDonell
 module AasmProgressable
+  # Internal class to manage display attribute associated with each
+  # model state, such as the name to display, and if the state has
+  # been completed yet or not.
   class State
-    #
-    attr_reader :name
-    attr_reader :status
+    attr_reader :name    # name displayed to the user
+    attr_reader :status  # :complete, :active, or :incomplete
 
     def initialize(name, status)
       @name = name
       @status = status
     end
 
+    # Returns an Array of State instances corresponding to the states
+    # of a model instance. The State instances are returned in the order
+    # given by the `aasm_state_order` declaration in the model.
     def self.create_all(object)
       localizer = AASM::Localizer.new
+
+      # Have we reached the current state yet?
+      #  Completed states precede the current state, so this is
+      #  the inverse of `is this state completed`
       found_current_state = false
 
-      #
       object.aasm_state_order.map do |state|
         is_current_state = state == object.aasm.current_state
         found_current_state = true if is_current_state
@@ -37,9 +47,7 @@ module AasmProgressable
   end
 
   module Helper
-    # ...
-    #
-    # @author Brendan MacDonell
+    # Renders a state indicator for a specified model instance.
     def render_state_indicator(object)
       states = State.create_all(object)
       rendered = render partial: 'aasm_progressable/states/list',
--- lib/aasm_progressable/model_mixin.rb
+++ lib/aasm_progressable/model_mixin.rb
@@ -1,14 +1,19 @@
-# TODO
+# Mixin for models with linear AASM-defined workflows.
+#
+# This mixin provides the aasm_state_order class settor, as well as
+# a similarly-named instance gettor.
 #
 # @author Brendan MacDonell
 module AasmProgressable
   module ModelMixin
     extend ActiveSupport::Concern
 
+    # Instance gettor to return the state order for the model
     def aasm_state_order
       self.class.get_aasm_state_order
     end
 
+    # Class gettors and settors for the state order
     module ClassMethods
       def aasm_state_order order
         @state_order = order
