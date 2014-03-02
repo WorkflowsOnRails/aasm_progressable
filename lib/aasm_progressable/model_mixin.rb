f836e1c921e63c95a3b324370d34fdd87c9372c5
Brendan MacDonell <brendan@macdonell.net>
2014-03-01 22:01:35 -0500

    Add helpers that make use of state ordering

--- lib/aasm_progressable/model_mixin.rb
+++ lib/aasm_progressable/model_mixin.rb
@@ -13,6 +13,24 @@ module AasmProgressable
       self.class.get_aasm_state_order
     end
 
+    def have_completed?(state)
+      current_index, target_index = state_index(self.aasm.current_state, state)
+      current_index > target_index
+    end
+
+    def have_not_completed?(state)
+      not have_completed?(state)
+    end
+
+    def have_started?(state)
+      current_index, target_index = state_index(self.aasm.current_state, state)
+      current_index >= target_index
+    end
+
+    def have_not_started?(state)
+      not have_started?(state)
+    end
+
     # Class gettors and settors for the state order
     module ClassMethods
       def aasm_state_order order
@@ -22,6 +40,14 @@ module AasmProgressable
       def get_aasm_state_order
         @state_order
       end
+    end
+
+    private
+
+    # Returns an array of zero-index state indices, where each index indicates
+    # the state's position in the ordered state collection.
+    def state_index(*states)
+      states.map { |state| self.aasm_state_order.index(state) }
     end
   end
 end
