7437ef1d558d7c60185ca476eb19b9da657babe2
Alexander Clelland <Argonanth@gmail.com>
2014-04-07 22:58:39 -0400

    Tests should be fine... implementation is broken
    For some reason the assignments of current_state and target_state are completely wrong...
    Initially current_state = 1, target_state = 0 for have_complete?(:one)

--- spec/progressable_spec.rb
+++ spec/progressable_spec.rb
@@ -2,32 +2,42 @@
 
 require 'spec_helper'
 
-class Fake
+class StateKlass
   include ActiveModel::Model
-
   include AASM
   include AasmProgressable::ModelMixin
-
   attr_accessor :aasm_state
-
   aasm do
     state :one, initial: true
     state :two
-
-    event :foo do
+    event :transition do
       transitions from: :one, to: :two
     end
   end
-
   aasm_state_order [:one, :two]
+end
+
+describe StateKlass do
+  test = StateKlass.new
+
+  it "Initial" do
+    expect(test.have_not_completed? :one).to eq true
+    expect(test.have_not_completed? :two).to eq true
+
+    expect(test.have_started? :one).to eq true
+    expect(test.have_not_started? :two).to eq true
+  end
+
+  test.transition #transition to state two
+
+  it "After Transition" do
+    expect(test.have_completed? :one).to eq true
+    expect(test.have_not_completed? :two).to eq true
+
+    expect(test.have_started? :one).to eq true
+    expect(test.have_started? :two).to eq true
+  end
 
 end
 
-describe "blah" do
-  @blah = Fake.new
 
-  binding.pry
-
-  #render_state_indicator(@blah)
-
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,4 @@ SimpleCov.start do
   add_filter "/spec/"
 end
 
-require_relative '../app/helpers/aasm_progressable/helper'
 require_relative '../lib/aasm_progressable/model_mixin'
